279a6fd4cbdaab1913c892958240851d9ad23439
dev-jam <mail@bobrosbag.nl>
2025-07-18 10:57:22 +0200

    license cleanup

--- opensesame_plugins/screenshot/screenshot/screenshot.py
+++ opensesame_plugins/screenshot/screenshot/screenshot.py
@@ -1,7 +1,22 @@
 """
-No rights reserved. All files in this repository are released into the public
-domain.
+This file is part of OpenSesame.
+
+OpenSesame is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+OpenSesame is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with OpenSesame.  If not, see <http://www.gnu.org/licenses/>.
 """
+
+__author__ = "Bob Rosbag"
+__license__ = "GPLv3"
 
 from libopensesame.py3compat import *
 from libopensesame.item import Item
